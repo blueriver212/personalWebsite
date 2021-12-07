9d57a9e7460c61a4f2cfcf77c495b6c45ba6a16c
blueriver212 <indy.brownhall@gmail.com>
2021-12-07 17:49:41 +0000

    Feature: Setting up a first django project

---
+++ mysite/website/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Question #not actually possible because i have not created it
---
+++ mysite/website/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('index', views.index, name="index"), # so this would be found at mysite/ >> name will call the view function
+    path('', views.landing, name="landing"),
+    path('about', views.about, name="about"),
+    path('projects', views.projects, name="projects"),
+    path('cv', views.cv, name="cv"),
+    path('blog', views.blog, name="blog"),
+]
