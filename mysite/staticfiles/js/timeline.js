27d00f8f98c9fd0943f0544fbdaf47c7443ba745
blueriver212 <indy.brownhall@gmail.com>
2021-12-09 15:32:00 +0000

    yeah

---
+++ mysite/staticfiles/js/timeline.js
@@ -0,0 +1,76 @@
+function getEducationOnly() {
+    document.getElementById('informed').style.display = "none";
+    document.getElementById('informed').style.display = "hidden";
+
+    document.getElementById('gather').style.display = "none";
+    document.getElementById('gather').style.display = "hidden";
+
+    document.getElementById('mma').style.display = "none";
+    document.getElementById('mma').style.display = "hidden";
+
+    document.getElementById('undp').style.display = "none";
+    document.getElementById('undp').style.display = "hidden";
+
+    document.getElementById('itc').style.display = "none";
+    document.getElementById('itc').style.display = "hidden";
+
+    // set education to show
+
+    document.getElementById('ucl').style.display = "block";
+    document.getElementById('ucl').style.display = "visable";
+
+    document.getElementById('durham').style.display = "block";
+    document.getElementById('durham').style.display = "visable";
+
+
+}
+
+function getAll() {
+    document.getElementById('informed').style.display = "block";
+    document.getElementById('informed').style.display = "visable";
+
+    document.getElementById('gather').style.display = "block";
+    document.getElementById('gather').style.display = "visable";
+
+    document.getElementById('mma').style.display = "block";
+    document.getElementById('mma').style.display = "visable";
+
+    document.getElementById('undp').style.display = "block";
+    document.getElementById('undp').style.display = "visable";
+
+    document.getElementById('itc').style.display = "block";
+    document.getElementById('itc').style.display = "visable";
+
+    // set education to show
+
+    document.getElementById('ucl').style.display = "block";
+    document.getElementById('ucl').style.display = "visable";
+
+    document.getElementById('durham').style.display = "block";
+    document.getElementById('durham').style.display = "visable";
+}
+
+function getWorkExperienceOnly() {
+    document.getElementById('informed').style.display = "block";
+    document.getElementById('informed').style.display = "visable";
+
+    document.getElementById('gather').style.display = "block";
+    document.getElementById('gather').style.display = "visable";
+
+    document.getElementById('mma').style.display = "block";
+    document.getElementById('mma').style.display = "visable";
+
+    document.getElementById('undp').style.display = "block";
+    document.getElementById('undp').style.display = "visable";
+
+    document.getElementById('itc').style.display = "block";
+    document.getElementById('itc').style.display = "visable";
+
+    // set education to show
+
+    document.getElementById('ucl').style.display = "none";
+    document.getElementById('ucl').style.display = "hidden";
+
+    document.getElementById('durham').style.display = "none";
+    document.getElementById('durham').style.display = "hidden";
+}
